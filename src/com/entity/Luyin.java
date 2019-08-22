c65019dc47820da3547f0cc91ab921cbc3fd6e49
zhang45258 <87176143@qq.com>
2019-08-22 16:28:08 +0800

    Create Luyin.java

---
+++ src/com/entity/Luyin.java
@@ -0,0 +1,82 @@
+package com.entity;
+ 
+public class Luyin {
+   
+    private String callid;
+    private String callerno;
+    private String calleeno;
+    private int agentid;
+    private String begintime;
+    private String endtime;
+    private String filename;
+    private String currentskillid;
+    private String content;//ÆÀ¼Û
+    private String contentchina;//ÆÀ¼Û
+    
+	public String getContentchina() {
+		return contentchina;
+	}
+	public void setContentchina(String contentchina) {
+		this.contentchina = contentchina;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCallid() {
+		return callid;
+	}
+	public void setCallid(String callid) {
+		this.callid = callid;
+	}
+	public String getCallerno() {
+		return callerno;
+	}
+	public void setCallerno(String callerno) {
+		this.callerno = callerno;
+	}
+	public String getCalleeno() {
+		return calleeno;
+	}
+	public void setCalleeno(String calleeno) {
+		this.calleeno = calleeno;
+	}
+	public int getAgentid() {
+		return agentid;
+	}
+	public void setAgentid(int agentid) {
+		this.agentid = agentid;
+	}
+	public String getBegintime() {
+		return begintime;
+	}
+	public void setBegintime(String begintime) {
+		this.begintime = begintime;
+	}
+	public String getEndtime() {
+		return endtime;
+	}
+	public void setEndtime(String endtime) {
+		this.endtime = endtime;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getCurrentskillid() {
+		return currentskillid;
+	}
+	public void setCurrentskillid(String currentskillid) {
+		this.currentskillid = currentskillid;
+	}
+    
+    
+    
+    
+    
+    
+}
