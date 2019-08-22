2aab9905626b403be120eceadb210dfba5349e34
zhang45258 <87176143@qq.com>
2019-08-22 16:28:00 +0800

    Create jiexu.java

---
+++ src/com/entity/jiexu.java
@@ -0,0 +1,83 @@
+package com.entity;
+
+public class jiexu {
+	private int AGENTID;
+	private String qianrushichang;
+	private String shimangshichang;
+	private String zhenglishichang;
+	private String tonghuashichang;
+	private String gongzuozongshichang;
+	private String dengdaishichang;
+	private int dengdaimiao;
+	private String gongshiliyonglv;
+	private String feigongzuoshichang;
+	private String name;
+	
+	public int getDengdaimiao() {
+		return dengdaimiao;
+	}
+	public void setDengdaimiao(int dengdaimiao) {
+		this.dengdaimiao = dengdaimiao;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAGENTID() {
+		return AGENTID;
+	}
+	public void setAGENTID(int aGENTID) {
+		AGENTID = aGENTID;
+	}
+	public String getQianrushichang() {
+		return qianrushichang;
+	}
+	public void setQianrushichang(String qianrushichang) {
+		this.qianrushichang = qianrushichang;
+	}
+	public String getShimangshichang() {
+		return shimangshichang;
+	}
+	public void setShimangshichang(String shimangshichang) {
+		this.shimangshichang = shimangshichang;
+	}
+	public String getZhenglishichang() {
+		return zhenglishichang;
+	}
+	public void setZhenglishichang(String zhenglishichang) {
+		this.zhenglishichang = zhenglishichang;
+	}
+	public String getTonghuashichang() {
+		return tonghuashichang;
+	}
+	public void setTonghuashichang(String tonghuashichang) {
+		this.tonghuashichang = tonghuashichang;
+	}
+	public String getGongzuozongshichang() {
+		return gongzuozongshichang;
+	}
+	public void setGongzuozongshichang(String gongzuozongshichang) {
+		this.gongzuozongshichang = gongzuozongshichang;
+	}
+	public String getDengdaishichang() {
+		return dengdaishichang;
+	}
+	public void setDengdaishichang(String dengdaishichang) {
+		this.dengdaishichang = dengdaishichang;
+	}
+	public String getGongshiliyonglv() {
+		return gongshiliyonglv;
+	}
+	public void setGongshiliyonglv(String gongshiliyonglv) {
+		this.gongshiliyonglv = gongshiliyonglv;
+	}
+	public String getFeigongzuoshichang() {
+		return feigongzuoshichang;
+	}
+	public void setFeigongzuoshichang(String feigongzuoshichang) {
+		this.feigongzuoshichang = feigongzuoshichang;
+	}
+	
+}
