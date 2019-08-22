99e0c7917be002294b62b57dbe29c5493f8bff47
zhang45258 <87176143@qq.com>
2019-08-22 16:27:53 +0800

    Create Jietonglv.java

---
+++ src/com/entity/Jietonglv.java
@@ -0,0 +1,35 @@
+package com.entity;
+
+public class Jietonglv {
+	private String shiduan;
+    private int rengongshurushu;
+	private int shijijietongshu;
+    private String rengongjietonglv;
+	public String getShiduan() {
+		return shiduan;
+	}
+	public void setShiduan(String shiduan) {
+		this.shiduan = shiduan;
+	}
+	public int getRengongshurushu() {
+		return rengongshurushu;
+	}
+	public void setRengongshurushu(int rengongshurushu) {
+		this.rengongshurushu = rengongshurushu;
+	}
+	public int getShijijietongshu() {
+		return shijijietongshu;
+	}
+	public void setShijijietongshu(int shijijietongshu) {
+		this.shijijietongshu = shijijietongshu;
+	}
+	public String getRengongjietonglv() {
+		return rengongjietonglv;
+	}
+	public void setRengongjietonglv(String rengongjietonglv) {
+		this.rengongjietonglv = rengongjietonglv;
+	}
+
+    
+    
+}
