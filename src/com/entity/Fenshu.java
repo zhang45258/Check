70f013c842e97e75827bed06ad5d8e93085973be
zhang45258 <87176143@qq.com>
2019-08-22 16:27:46 +0800

    Create Fenshu.java

---
+++ src/com/entity/Fenshu.java
@@ -0,0 +1,57 @@
+package com.entity;
+
+public class Fenshu {
+    private String callid;
+    private String agentid;
+    //private String begintime;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private String biaoqian1;
+	public String getCallid() {
+		return callid;
+	}
+	public void setCallid(String callid) {
+		this.callid = callid;
+	}
+	public String getAgentid() {
+		return agentid;
+	}
+	public void setAgentid(String agentid) {
+		this.agentid = agentid;
+	}
+
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
+	public int getD() {
+		return d;
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+	public String getBiaoqian1() {
+		return biaoqian1;
+	}
+	public void setBiaoqian1(String biaoqian1) {
+		this.biaoqian1 = biaoqian1;
+	}
+	
+    
+}
